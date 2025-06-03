ca311b8ebacc580075cc7bfdcd20e614e1889587
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-03 16:11:41 -0400

    Initial Commit

---
+++ Tempus/TempusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempusApp.swift
+//  Tempus
+//
+//  Created by Marcell Fulop on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TempusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

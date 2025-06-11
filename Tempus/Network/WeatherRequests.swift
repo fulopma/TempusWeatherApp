a7184cea87211abb2da5e3609dbcb7752e4a721e
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-11 14:00:12 -0400

    added temp now

--- Tempus/Network/WeatherRequests.swift
+++ Tempus/Network/WeatherRequests.swift
@@ -5,7 +5,9 @@
 //  Created by Marcell Fulop on 6/3/25.
 //
 
-// https://archive-api.open-meteo.com/v1/archive?latitude=33.9091&longitude=-84.479&start_date=2025-01-14&end_date=2025-01-14&hourly=temperature_2m&timezone=America%2FNew_York
+// https://archive-api.open-meteo.com/v1/archive?latitude=33.9091&longitude=-84.479&start_date=2025-01-14&end_date=2025-01-14&hourly=temperature_2m
+
+/// All Weather Requests and Responses are UTC-0 and NOT adjusted for local time.
 
 import NetworkLayer
 
@@ -16,39 +18,54 @@ struct TemperatureHistoryRequest: Request {
     var params: [String : String]
     var header: [String : String] = [:]
     
-    static func createRequest(date: String, latitude: Double, longitude: Double, timeZone: String) -> TemperatureHistoryRequest {
+    static func createRequest(date: String, latitude: Double, longitude: Double) -> TemperatureHistoryRequest {
         let params: [String: String] = [
             "latitude": String(latitude),
             "longitude": String(longitude),
             "start_date": date,
             "end_date": date,
-            "hourly": "temperature_2m",
-            "timezone": timeZone
+            "hourly": "temperature_2m"
         ]
         return TemperatureHistoryRequest(params: params)
     }
 }
 
-// https://archive-api.open-meteo.com/v1/archive?latitude=33.9091&longitude=-84.479&start_date=2025-01-01&end_date=2025-01-14&daily=precipitation_sum&timezone=America%2FNew_York
+// https://archive-api.open-meteo.com/v1/archive?latitude=33.9091&longitude=-84.479&start_date=2025-01-01&end_date=2025-01-14&daily=precipitation_sum
 struct PrecipitationHistoryRequest: Request {
     var baseURL: String = "https://archive-api.open-meteo.com"
     var path: String = "/v1/archive"
-    
     var httpMethod: HttpMethod = .get
-    
     var params: [String : String]
-    
     var header: [String : String] = [:]
     
-    static func createRequest(date: String, latitude: Double, longitude: Double, timeZone: String) -> PrecipitationHistoryRequest {
+    static func createRequest(date: String, latitude: Double, longitude: Double) -> PrecipitationHistoryRequest {
         let params: [String: String] = [
             "latitude": String(latitude),
             "longitude": String(longitude),
             "start_date": date,
             "end_date": date,
             "daily": "precipitation_sum",
-            "timezone": timeZone
         ]
         return PrecipitationHistoryRequest(params: params)
     }
+}
+
+// https://api.open-meteo.com/v1/forecast?latitude=37.77&longitude=122.42&hourly=temperature_2m&forecast_days=1
+struct TemperatureNowRequest: Request {
+    var baseURL: String = "https://api.open-meteo.com"
+    var path: String = "/v1/forecast"
+    var httpMethod: HttpMethod = .get
+    var params: [String : String]
+    var header: [String : String] = [:]
+    static func createRequest(latitude: Double, longitude: Double) -> TemperatureNowRequest
+    {
+        let params: [String: String] = [
+            "latitude": String(latitude),
+            "longitude": String(longitude),
+            "hourly": "temperature_2m",
+            "forecast_days": "1"
+        ]
+        return TemperatureNowRequest(params: params)
+    }
+    
 }

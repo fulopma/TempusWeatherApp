97ace883543a2ca7c2d952bc9418f57d52ddc9c2
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-11 13:58:13 -0400

    Added coordinator

--- Tempus/Coordinator.swift
+++ Tempus/Coordinator.swift
@@ -5,3 +5,23 @@
 //  Created by Marcell Fulop on 6/4/25.
 //
 
+import SwiftUI
+//
+//enum Screen: View {
+//    case welcome = WelcomeView()
+//    case 
+//}
+
+@main
+struct Coordinator: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            #if DEBUG
+            WeatherSummaryView(latitude: 37.77, longitude: -122.419, city: "San Francisco")
+            #else
+            WelcomeView()
+            #endif
+        }
+    }
+}

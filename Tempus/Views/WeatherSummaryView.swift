91368515449022ed4b99967fe90a16f71b9d2f00
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-11 14:03:03 -0400

    Added simple temp reader

--- Tempus/Views/WeatherSummaryView.swift
+++ Tempus/Views/WeatherSummaryView.swift
@@ -4,5 +4,23 @@
 //
 //  Created by Marcell Fulop on 6/4/25.
 //
+import SwiftUI
 
+struct WeatherSummaryView: View {
+    @ObservedObject var weatherSummaryVM: WeatherSummaryViewModel
+    init(latitude: Double, longitude: Double, city: String) {
+        self.weatherSummaryVM = WeatherSummaryViewModel(latitude: latitude, longitude: longitude, city: city)
+    }
+    var body: some View {
+        VStack {
+            Text(weatherSummaryVM.city)
+            Text(weatherSummaryVM.getTemperatureFormatted())
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WeatherSummaryView(latitude: 37.77, longitude: 122.42, city: "San Francisco")
+}
 

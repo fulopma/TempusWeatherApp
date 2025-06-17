06324426f97c3eea0e30c9ac33456f2f577ae968
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-06-16 23:32:57 -0400

    wrote a few test cases for unit conversions

--- TempusTests/TempusTests.swift
+++ TempusTests/TempusTests.swift
@@ -9,4 +9,37 @@ import XCTest
 @testable import Tempus
 
 final class TempusTests: XCTestCase {
+    func testGetTemperatureUnit() {
+        XCTAssertEqual(Units.usCustomary.getTemperatureUnit(), "°F")
+        XCTAssertEqual(Units.metric.getTemperatureUnit(), "°C")
+        XCTAssertEqual(Units.scientific.getTemperatureUnit(), "K")
+    }
+    func testGetPrecipationUnit() {
+        XCTAssertEqual(Units.usCustomary.getPrecipationUnit(), "in")
+        XCTAssertEqual(Units.metric.getPrecipationUnit(), "mm")
+        XCTAssertEqual(Units.scientific.getPrecipationUnit(), "m")
+    }
+    func testConvertPrecipitation() {
+        // 10 mm to in
+        XCTAssertEqual(Units.usCustomary.convertPrecipitation(fromValue: 10), 0.393701, accuracy: 0.0001)
+        // 10 mm to mm
+        XCTAssertEqual(Units.metric.convertPrecipitation(fromValue: 10), 10, accuracy: 0.0001)
+        // 10 mm to m
+        XCTAssertEqual(Units.scientific.convertPrecipitation(fromValue: 10), 0.01, accuracy: 0.0001)
+    }
+    /// All temperature is internally stored as celsius but can displayed in whatever unit system
+    func testConvertTemperature() {
+        // 0°C to °F
+        XCTAssertEqual(Units.usCustomary.convertTemperature(fromValue: 0), 32, accuracy: 0.0001)
+        // 100°C to °F
+        XCTAssertEqual(Units.usCustomary.convertTemperature(fromValue: 100), 212, accuracy: 0.0001)
+        // 0°C to °C
+        XCTAssertEqual(Units.metric.convertTemperature(fromValue: 0), 0, accuracy: 0.0001)
+        // 100°C to °C
+        XCTAssertEqual(Units.metric.convertTemperature(fromValue: 100), 100, accuracy: 0.0001)
+        // 0°C to K
+        XCTAssertEqual(Units.scientific.convertTemperature(fromValue: 0), 273.15, accuracy: 0.0001)
+        // 100°C to K
+        XCTAssertEqual(Units.scientific.convertTemperature(fromValue: 100), 373.15, accuracy: 0.0001)
+    }
 }
